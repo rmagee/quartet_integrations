c42e9a6722e5c028f1ead825302ef02fcd0a34ad
Serial Lab <slab@serial-lab.com>
2019-07-29 11:52:16 -0600

    removed compiled requirements since they were overriding the default quartet package's during install.

--- compile-requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-pip-compile --output-file requirements.txt requirements.in --upgrade
-pip-compile --output-file requirements_dev.txt requirements_dev.in --upgrade
-pip-compile --output-file requirements_test.txt requirements_test.in --upgrade
-
-
