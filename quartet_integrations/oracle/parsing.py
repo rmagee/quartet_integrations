7f19e12c95ce2d955ccd93030b9978d03507f15f
Serial Lab <slab@serial-lab.com>
2019-10-28 12:31:33 -0600

    Oracle master material input working revision.

---
+++ quartet_integrations/oracle/parsing.py
@@ -0,0 +1,70 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2019 SerialLab Corp.  All rights reserved.
+
+import pandas
+from io import BytesIO
+from copy import copy
+from quartet_capture.rules import Step, RuleContext
+from quartet_masterdata.models import TradeItem, Company, TradeItemField
+
+
+class MasterMaterialParser:
+    """
+    Imports master material exports from oracle into the quartet trade
+    items.
+    """
+
+    def __init__(self, company_records: dict):
+        self.company_records = company_records
+
+    def parse(self, data: bytes, info_func, rule_context: RuleContext = None):
+        file_stream = BytesIO(data)
+        parsed_data = pandas.read_excel(
+            file_stream,
+            sheet_name='Sheet1',
+            dtype=str
+        )
+        print(parsed_data)
+        for row in parsed_data.values:
+            self.create_trade_item(row[0], row[1], row[2], pallet_pack=row[8])
+            self.create_trade_item(row[0], row[3], row[4], row[5], row[8])
+            if row[6]:
+                self.create_trade_item(row[0], row[6], row[7], row[8])
+
+    def create_trade_item(self, material_number, unit_of_measure, gtin14,
+                          pack_count=None, pallet_pack=None):
+        company = self.get_company(gtin14)
+        trade_item = TradeItem.objects.get_or_create(
+            company=company,
+            additional_id=material_number,
+            package_uom=unit_of_measure,
+            GTIN14=gtin14,
+            pack_count=pack_count
+        )
+        TradeItemField.objects.get_or_create(
+            trade_item=trade_item,
+            name='pallet_pack_count',
+            value=pallet_pack
+        )
+
+    def get_company(self, gtin: str):
+        """
+        Looks for the company prefix in the GTIN and returns the company
+        database model if that is found.
+        :param gtin: The gtin to inspect
+        :return: A quartet_masterdata.models.Company instance.
+        """
+        for company, db_company in self.company_records.items():
+            if company in gtin: return db_company
---
+++ quartet_integrations/oracle/steps.py
@@ -0,0 +1,67 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2019 SerialLab Corp.  All rights reserved.
+from quartet_capture import models
+from quartet_capture.rules import Step, RuleContext
+from quartet_integrations.oracle.parsing import MasterMaterialParser
+import pandas
+from io import BytesIO
+from quartet_capture.rules import Step, RuleContext
+from quartet_masterdata.models import Company
+
+
+class TradeItemImportStep(Step):
+    """
+    Imports master material exports from oracle into the quartet trade
+    items.
+    """
+
+    def __init__(self, db_task: models.Task, **kwargs):
+        super().__init__(db_task, **kwargs)
+        param = self.get_or_create_parameter(
+            'Company Prefix 1', '0347771',
+            'A company prefix of the company we are importing for. '
+            'Trade items must have a company associated with them so '
+            'companies must be set up prior to running the import.'
+        )
+        self.parameters['Company Prefix 1'] = param
+
+    def execute(self, data, rule_context: RuleContext):
+        self.info('Invoking the parser.')
+        company_records = self.get_company_records()
+        MasterMaterialParser(company_records).parse(
+            data, self.info, rule_context
+        )
+
+    def get_company_records(self):
+        company_records = {}
+        for name, value in self.parameters.items():
+            if name.startswith('Company Prefix'):
+                company_records[value] = Company.objects.get(
+                    gs1_company_prefix=value
+                )
+        return company_records
+
+    @property
+    def declared_parameters(self):
+        return {
+            'Company_X': 'There can be n number of "Company_X" records, '
+                         'for example: Company_1 with a value of "0347771",'
+                         'Company_2 with a value of "0345551", etc.  The '
+                         'import will look for these company records in '
+                         'order to associate with trade items.'
+        }
+
+    def on_failure(self):
+        pass
