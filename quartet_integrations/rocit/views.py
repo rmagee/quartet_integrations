e2819e261f6dbd3d43b2a75a7f1c8399544cf3d1
Serial Lab <slab@serial-lab.com>
2020-03-05 11:54:32 -0700

    Updated logic to find WSDLs due to refactoring, etc.

--- quartet_integrations/rocit/views.py
+++ quartet_integrations/rocit/views.py
@@ -112,10 +112,13 @@ class RetrievePackagingHierarchyView(RocItBaseView):
 
     def get(self, request: Request):
         ret = None
-        if 'wsdl' in self.request.query_params.keys():
+        if request.query_params.get('wsdl') :
             ret = self.get_wsdl('wsdl')
+        elif request.query_params.get('WSDL'):
+            ret = self.get_wsdl(request.query_params.get("WSDL"))
         elif request.query_params.get('XSD'):
             ret = self.get_wsdl('XSD')
+
         return ret or Response("Resource not found.",
                  status.HTTP_404_NOT_FOUND, content_type="*/*")
 
