7c5f0912face5724715d9a00620fb267be057477
Serial Lab <slab@serial-lab.com>
2021-02-19 13:36:20 -0700

    Updated tracelink SNX unit tests.

--- tests/test_tracelink_snx.py
+++ tests/test_tracelink_snx.py
@@ -15,28 +15,31 @@
 from logging import getLogger
 
 import os
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from quartet_templates.models import Template
+
 from quartet_capture.models import Rule, Step, StepParameter
-from random_flavorpack.management.commands.load_test_random_pools import \
-    Command
+from quartet_templates.models import Template
 from random_flavorpack.models import RandomizedRegion
-from serialbox.management.commands.load_test_pools import Command as test_pools
-from serialbox.management.commands.load_serialbox_auth import Command as load_auth
+from serialbox.management.commands.load_serialbox_auth import \
+    Command as load_auth
 from serialbox.models import Pool, ResponseRule, SequentialRegion
+
 logger = getLogger(__name__)
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import Group, Permission, ContentType
+from serialbox import models
 
 
-class GuardianTestCase(APITestCase):
+
+class TraceLinkSNXTestCase(APITestCase):
     def setUp(self):
         user = User.objects.create_user(username='testuser',
                                         password='unittest',
                                         email='testuser@seriallab.local')
-        # this creates teh pool api access group
-        Command().handle()
-        test_pools().handle()
+        self.create_random_pool()
+        self.create_sequential_pool()
         load_auth().handle()
         group = Group.objects.get(name='Pool API Access')
         user.groups.add(group)
@@ -44,14 +47,97 @@ class GuardianTestCase(APITestCase):
         self.client.force_authenticate(user=user)
         self.create_template()
         self.rule = self.create_rule()
+        self.create_permissions()
         self.create_response_rule()
         self.create_random_template()
         self.create_random_sscc_template()
+        self.create_sequential_sscc_template()
         self.random_sscc_rule = self.create_random_sscc_rule()
+        self.sequential_sscc_rule = self.create_sequential_sscc_rule()
         self.random_rule = self.create_random_rule()
         self.create_random_response_rule()
         self.create_sscc_pool()
         self.create_random_sscc_pool()
+
+    def create_permissions(self, *args, **options):
+        ct = ContentType.objects.get_for_model(models.Pool)
+        pool_allocate, created = Permission.objects.get_or_create(
+            codename='allocate_numbers',
+            content_type=ct
+        )
+        if created:
+            pool_allocate.name='Can allocate numbers'
+            pool_allocate.save()
+
+        group = Group.objects.get_or_create(
+            name='Pool API Access'
+        )[0]
+        allocate_group = Group.objects.get_or_create(
+            name='Allocate Numbers Access'
+        )[0]
+        self._add_permission(allocate_group, pool_allocate)
+        self._add_permission(group,
+           pool_allocate
+        )
+        self._add_permission(group,
+            Permission.objects.get(codename='add_pool')
+        )
+        self._add_permission(group,
+            Permission.objects.get(codename='change_pool')
+        )
+        self._add_permission(group,
+            Permission.objects.get(codename='delete_pool')
+        )
+        self._add_permission(group,
+            Permission.objects.get(codename='add_sequentialregion')
+        )
+        self._add_permission(group,
+            Permission.objects.get(codename='change_sequentialregion')
+        )
+        self._add_permission(group,
+            Permission.objects.get(codename='delete_sequentialregion')
+        )
+        for perm in group.permissions.all():
+            print(perm.name)
+
+    def _add_permission(self, group: Group, permission: Permission):
+        group.permissions.add(permission)
+
+
+    def create_sequential_pool(self):
+        sp1 = models.Pool.objects.create(
+            readable_name=_('Logositol 100mg Cartons'),
+            machine_name='00377700000136',
+            active=True,
+            request_threshold=1000
+        )
+        models.SequentialRegion.objects.create(
+            readable_name=_('Logositol 100mg Cartons'),
+            machine_name='00377700000136',
+            order=1,
+            start=1,
+            end=9999999999,
+            pool=sp1
+        )
+
+    def create_random_pool(self):
+        # this creates teh pool api access group
+        sp1 = Pool.objects.create(
+            readable_name=_('Pharmaprod 20mcg Pills'),
+            machine_name='00313000007772',
+            active=True,
+            request_threshold=1000
+        )
+        RandomizedRegion.objects.create(
+            readable_name=_('Pharmaprod 20mcg Pills'),
+            machine_name='00313000007772',
+            start=239380,
+            active=True,
+            order=1,
+            pool=sp1,
+            min=1,
+            max=999999999999
+        )
 
     def create_sscc_pool(self):
         pool = Pool.objects.create(
@@ -68,7 +154,7 @@ class GuardianTestCase(APITestCase):
             pool=pool
         )
         ResponseRule.objects.create(
-            rule=self.rule,
+            rule=self.sequential_sscc_rule,
             pool=pool,
             content_type='xml'
         )
@@ -104,6 +190,7 @@ class GuardianTestCase(APITestCase):
                 description='The tracelink response template'
             )
 
+
     def create_random_template(self):
         curpath = os.path.dirname(__file__)
         data_path = os.path.join(curpath,
@@ -124,6 +211,18 @@ class GuardianTestCase(APITestCase):
             content = f.read()
             Template.objects.create(
                 name='TraceLink Random SSCC Response',
+                content=content,
+                description='The tracelink random response template'
+            )
+
+    def create_sequential_sscc_template(self):
+        curpath = os.path.dirname(__file__)
+        data_path = os.path.join(curpath,
+                                 'data/tracelink/SN_Response_SSCC_Range.xml')
+        with open(data_path, 'r') as f:
+            content = f.read()
+            Template.objects.create(
+                name='TraceLink Sequential SSCC Response',
                 content=content,
                 description='The tracelink random response template'
             )
@@ -186,6 +285,32 @@ class GuardianTestCase(APITestCase):
         )
         return rule
 
+    def create_sequential_sscc_rule(self):
+        rule = Rule.objects.create(
+            name='TraceLink Sequential SSCC Number Reply',
+            description='unit test'
+        )
+        Step.objects.create(
+            name="Convert to SSCC 18",
+            rule=rule,
+            step_class='quartet_integrations.serialbox.'
+                       'steps.ListToBarcodeConversionStep',
+            description='Converts raw serialbox numbers to SSCC-18 values',
+            order=1
+        )
+        template_step = Step.objects.create(
+            name='Format With Template',
+            rule=rule,
+            step_class='quartet_templates.steps.TemplateStep',
+            description='unit test step',
+            order=2
+        )
+        StepParameter.objects.create(
+            name='Template Name',
+            value='TraceLink Sequential SSCC Response',
+            step=template_step
+        )
+        return rule
 
     def create_random_sscc_rule(self):
         rule = Rule.objects.create(
@@ -252,7 +377,7 @@ class GuardianTestCase(APITestCase):
             print(result.data)
             self.assertEqual(result.status_code, 200)
 
-    def test_post_sscc_request(self):
+    def test_post_sequential_sscc_request(self):
         """
         Posts an GTIN request to the system using the Tracelink format.
 
@@ -260,7 +385,7 @@ class GuardianTestCase(APITestCase):
         curpath = os.path.dirname(__file__)
         file_path = os.path.join(
             curpath,
-            'data/tracelink/sscc_sequential_number_request.xml'
+            'data/tracelink/SN_Request_SSCC_Range.xml'
         )
         with open(file_path, 'r') as f:
             request = f.read()
