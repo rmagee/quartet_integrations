6726ecd5a861d7b6fd8f112cc641553ac55de6bc
Serial Lab <slab@serial-lab.com>
2021-02-24 09:25:24 -0700

    Added a debugging raw logger for connectivity testing.

---
+++ quartet_integrations/generic/views.py
@@ -0,0 +1,59 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2020 SerialLab Corp.  All rights reserved.
+from os import path
+from django.conf import settings
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_xml.renderers import XMLRenderer
+from quartet_capture.parsers import RawParser
+import json
+
+class TaskXMLRenderer(XMLRenderer):
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return data
+
+class LoggingView(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+    renderer_classes = [TaskXMLRenderer]
+    parser_classes = [RawParser]
+
+    def post(self, request: Request, format=None):
+        """
+        This simply logs the full request including headers
+        """
+        stream = request.stream
+        raw_request = ["%s: %s" % (name, val) for name, val in stream.headers.items()]
+        raw_request = '\n'.join(raw_request)
+        raw_request = """
+
+
+
+**********************
+Headers: %s
+**********************
+
+Request Body:
+%s""" % (raw_request, request.data)
+        self.write_file(raw_request)
+        return Response(raw_request)
+
+    def write_file(self, raw_request):
+        log_file = path.join(settings.LOGGING_PATH, 'requests.txt')
+        with open(log_file, "w+") as f:
+            f.write(raw_request)
--- quartet_integrations/urls.py
+++ quartet_integrations/urls.py
@@ -5,11 +5,13 @@ from .rocit.urls import urlpatterns as rocit_urls
 from .systech.guardian.urls import urlpatterns as guardian_urls
 from .systech.unitrace.urls import urlpatterns as unitrace_urls
 from .tracelink.urls import urlpatterns as tracelink_urls
+from .generic.urls import urlpatterns as debugging_urls
 
 urlpatterns = [
     url(r'^', include(opsm_urls)),
     url(r'^', include(rocit_urls)),
     url(r'^', include(guardian_urls)),
     url(r'^', include(unitrace_urls)),
-    url(r'^', include(tracelink_urls))
+    url(r'^', include(tracelink_urls)),
+    url(r'^', include(debugging_urls))
 ]
