4483cda5e6daac577c423277446de6dd34e96a54
Serial Lab <slab@serial-lab.com>
2021-10-28 13:55:01 -0600

    :/

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'quartet_integrations'
-copyright = u'2018, SerialLab, Corp'
+copyright = u'2018-2021, SerialLab, Corp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
